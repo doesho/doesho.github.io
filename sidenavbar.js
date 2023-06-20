34424b7c33f2675e99d75806fda2dcd9e5f2915b
doesho <wririley@pm.me>
2023-06-20 13:12:57 -0700

    created side nav bar

--- sidenavbar.js
+++ sidenavbar.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+const opener = document.getElementById("SideNavOpener");
+const sideBarDiv = document.getElementById("SideNavBar");
+const closer = document.getElementById("SideNavCloser");
+
+opener.addEventListener("click", openSideBar);
+closer.addEventListener("click", closeSideBar);
+
+function openSideBar() {
+    sideBarDiv.style.width = "100px";
+    sideBarDiv.style.padding = "10px";
+    sideBarDiv.style.borderRight = "1px solid gray";
+}
+
+function closeSideBar() {
+    sideBarDiv.style.width = "0px";
+    sideBarDiv.style.padding = "0px";
+    sideBarDiv.style.borderRight = "none";
+}
