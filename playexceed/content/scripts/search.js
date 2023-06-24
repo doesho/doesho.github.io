a2c52c31215af0937a48f7733642f4725105224a
doesho <wririley@pm.me>
2023-06-23 17:50:37 -0700

    now only looks in choice container

--- playexceed/content/scripts/search.js
+++ playexceed/content/scripts/search.js
@@ -1,8 +1,9 @@
 //core search functionality. change style of matches?
 
 function funcSearch(input) {
+    let choiceContainer = document.getElementById("ChoiceUniqueContainer");
     let filter = input.target.value.toUpperCase();
-    let links = document.querySelectorAll("a.ChoiceLink");
+    let links = choiceContainer.querySelectorAll("a.ChoiceLink");
 
     for (let i = 0; i < links.length; i++) {
         let link = links[i];
