76bd907d93d24483c8728e62ad4d0396b2c49b62
doe <wririley@pm.me>
2023-06-24 15:29:13 -0700

    sidenav has random art at bottom

--- playexceed/content/scripts/sidenav.js
+++ playexceed/content/scripts/sidenav.js
@@ -2,12 +2,24 @@ const opener = document.getElementById("SideNavOpener");
 const sideNavDiv = document.getElementById("SideNav");
 const closer = document.getElementById("SideNavCloser");
 
+let currentImage;
+let imageArray = [
+    '../content/images/sidebar/orie.png',
+    '../content/images/sidebar/hazama.png',
+    '../content/images/sidebar/platinum.png',
+    '../content/images/sidebar/specter.png',
+    '../content/images/sidebar/tinker.png',
+    '../content/images/sidebar/shovel.png',
+
+]
+
 opener.addEventListener("click", openSideNav);
 document.addEventListener("click", closeSideNav);
 
 function openSideNav() { //width set auto, becomes child width (sideNavcontents in sideNavstyle.css)
     sideNavDiv.style.width = "auto";
     sideNavDiv.style.borderRight = "1px solid gray";
+    randomSideNavPic();
     event.stopPropagation();
 }
 
@@ -16,4 +28,14 @@ function closeSideNav() {
         sideNavDiv.style.width = "";
         sideNavDiv.style.borderRight = "";
     }
+}
+
+//retrieve random image when sidebar is opened
+
+function randomSideNavPic() {
+    randomInt = Math.floor(Math.random() * imageArray.length);
+    let currentImage = imageArray[randomInt];
+    let sideNavImage = document.getElementById('BottomPic');
+
+    sideNavImage.src = currentImage;
 }
