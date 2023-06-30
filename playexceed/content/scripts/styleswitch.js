bccbac0fa91e372dbb4e3c000ccf9ad0b4587bb3
doe <wririley@pm.me>
2023-06-29 18:52:35 -0700

    added lightmode toggle beta

--- playexceed/content/scripts/styleswitch.js
+++ playexceed/content/scripts/styleswitch.js
@@ -35,3 +35,40 @@ function styleSwitch() {
     styleStateIndex++;
     document.getElementById("ButtonText").innerHTML = currentState;
 }
+
+// lightmode darkmode. hard coded to remain this way.
+
+const modeButton = document.getElementById("ModeButton");
+const lightmode = document.getElementById("LightMode")
+const darkmode = document.getElementById("DarkMode")
+let modeStateIndex = 1;
+let modeState;
+
+if (darkmode && lightmode) {
+    darkmode.disabled = true;
+    modeState = "Light [this wont save]";
+    document.getElementById("ModeButtonText").innerHTML = modeState;
+}
+
+modeButton.addEventListener("click", modeSwitch);
+
+function modeSwitch() {
+    if (modeStateIndex > 1) {
+        modeStateIndex = 0;
+    }
+
+    switch (modeStateIndex) {
+        case 0:
+            lightmode.disabled = false;
+            darkmode.disabled = true;
+            modeState = "Light";
+            break;
+        case 1:
+            lightmode.disabled = true;
+            darkmode.disabled = false;
+            modeState = "Dark";
+            break;
+    }
+    modeStateIndex++;
+    document.getElementById("ModeButtonText").innerHTML = modeState;
+}
