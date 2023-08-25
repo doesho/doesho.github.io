84956fae64c1a0db6c7cc0cfa587cb68e00224e4
doesho <wririley@pm.me>
2023-08-25 13:21:35 -0700

    added community entry support

--- content/scripts/search.js
+++ content/scripts/search.js
@@ -29,6 +29,7 @@ let selectedCatagory = "";
 
 let choiceInterview = document.getElementsByClassName('Interview');
 let choiceOpinion = document.getElementsByClassName('Opinion');
+let choiceCommunity = document.getElementsByClassName('Community');
 let choiceNews = document.getElementsByClassName('News');
 
 selector.addEventListener("change", funcCatagory);
@@ -44,6 +45,9 @@ function funcCatagory() {
             for (let i = 0; i < choiceOpinion.length; i++) {
                 choiceOpinion[i].classList.add('hidden');
             }
+            for (let i = 0; i < choiceCommunity.length; i++) {
+                choiceCommunity[i].classList.add('hidden');
+            }
             for (let i = 0; i < choiceNews.length; i++) {
                 choiceNews[i].classList.add('hidden');
             }
@@ -55,6 +59,24 @@ function funcCatagory() {
             }
             for (let i = 0; i < choiceOpinion.length; i++) {
                 choiceOpinion[i].classList.remove('hidden');
+            }
+            for (let i = 0; i < choiceCommunity.length; i++) {
+                choiceCommunity[i].classList.add('hidden');
+            }
+            for (let i = 0; i < choiceNews.length; i++) {
+                choiceNews[i].classList.add('hidden');
+            }
+
+            break;
+        case 'Community':
+            for (let i = 0; i < choiceInterview.length; i++) {
+                choiceInterview[i].classList.add('hidden');
+            }
+            for (let i = 0; i < choiceOpinion.length; i++) {
+                choiceOpinion[i].classList.add('hidden');
+            }
+            for (let i = 0; i < choiceCommunity.length; i++) {
+                choiceCommunity[i].classList.remove('hidden');
             }
             for (let i = 0; i < choiceNews.length; i++) {
                 choiceNews[i].classList.add('hidden');
@@ -68,6 +90,9 @@ function funcCatagory() {
             for (let i = 0; i < choiceOpinion.length; i++) {
                 choiceOpinion[i].classList.add('hidden');
             }
+            for (let i = 0; i < choiceCommunity.length; i++) {
+                choiceCommunity[i].classList.add('hidden');
+            }
             for (let i = 0; i < choiceNews.length; i++) {
                 choiceNews[i].classList.remove('hidden');
             }
@@ -79,6 +104,9 @@ function funcCatagory() {
             }
             for (let i = 0; i < choiceOpinion.length; i++) {
                 choiceOpinion[i].classList.remove('hidden');
+            }
+            for (let i = 0; i < choiceCommunity.length; i++) {
+                choiceCommunity[i].classList.remove('hidden');
             }
             for (let i = 0; i < choiceNews.length; i++) {
                 choiceNews[i].classList.remove('hidden');
