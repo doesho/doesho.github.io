f267397e0431a88cc39c4a416f1a4c35626cb650
doesho <wririley@pm.me>
2023-09-01 10:52:39 -0700

    reimplement style switch

--- content/scripts/styleswitch.js
+++ content/scripts/styleswitch.js
@@ -2,12 +2,12 @@
 
 const button = document.getElementById("StyleButton");
 const lines = document.getElementById("LineSheet");
-const cubes = document.getElementById("CubeSheet");
+const compact = document.getElementById("CompactSheet");
 let styleStateIndex = 0;
 let currentState;
 
-if (cubes && lines) {
-    cubes.disabled = true;
+if (compact && lines) {
+    compact.disabled = true;
     currentState = "Default";
     document.getElementById("ButtonText").innerHTML = currentState;
 }
@@ -23,52 +23,15 @@ function styleSwitch() {
     switch (styleStateIndex) {
         case 0:
             lines.disabled = true;
-            cubes.disabled = false;
-            currentState = "Grid (Busted)";
+            compact.disabled = false;
+            currentState = "Compact";
             break;
         case 1:
-            cubes.disabled = true;
             lines.disabled = false;
+            compact.disabled = true;
             currentState = "Default";
             break;
     }
     styleStateIndex++;
     document.getElementById("ButtonText").innerHTML = currentState;
-}
-
-// lightmode darkmode. hard coded to remain this way.
-
-const modeButton = document.getElementById("ModeButton");
-const lightmode = document.getElementById("LightMode")
-const darkmode = document.getElementById("DarkMode")
-let modeStateIndex = 1;
-let modeState;
-
-if (darkmode && lightmode) {
-    darkmode.disabled = true;
-    modeState = "Light [this wont save]";
-    document.getElementById("ModeButtonText").innerHTML = modeState;
-}
-
-modeButton.addEventListener("click", modeSwitch);
-
-function modeSwitch() {
-    if (modeStateIndex > 1) {
-        modeStateIndex = 0;
-    }
-
-    switch (modeStateIndex) {
-        case 0:
-            lightmode.disabled = false;
-            darkmode.disabled = true;
-            modeState = "Light";
-            break;
-        case 1:
-            lightmode.disabled = true;
-            darkmode.disabled = false;
-            modeState = "Dark";
-            break;
-    }
-    modeStateIndex++;
-    document.getElementById("ModeButtonText").innerHTML = modeState;
 }
